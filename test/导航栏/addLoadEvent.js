d9c9c5bc2762ef502823e9e724b9412cfe84163d
noyalin <1375162594@qq.com>
2017-04-12 23:58:10 +0800

    2次更新

---
+++ test/导航栏/addLoadEvent.js
@@ -0,0 +1,14 @@
+/**
+ * Created by linyan on 16/4/23.
+ */
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
